a3eeca2f929359f013443eb7c46c16ea63dd62bf
Ainor Syahrizal <macintorch@gmail.com>
2016-03-30 21:43:14 +0800

    add devise omniauth facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
    
+  #get 'welcome/index'
+
+  devise_for :users, class_name: 'FormUser', controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations'}
    resources :messages
 
    root 'messages#index'
