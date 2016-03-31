91e7fa6b3a53b10bbad7fdc5dfe70c30a2fe5516
Ainor Syahrizal <macintorch@gmail.com>
2016-03-31 13:52:39 +0800

    styling and authentication controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
    
   #get 'welcome/index'
+  root 'messages#index'
 
   devise_for :users, class_name: 'FormUser', controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations'}
    resources :messages do
    		resources :comments 
    	end
 
-   root 'messages#index'
+   
 end
