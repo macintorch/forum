165e58ac507766f065260ff9b4819a44f0333a9f
Ainor Syahrizal <macintorch@gmail.com>
2016-03-31 11:21:39 +0800

    comment controller model view file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :messages
-  
+  has_many :messages, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   devise :omniauthable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   #get 'welcome/index'
 
   devise_for :users, class_name: 'FormUser', controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations'}
-   resources :messages
+   resources :messages do
+   		resources :comments 
+   	end
 
    root 'messages#index'
 end
